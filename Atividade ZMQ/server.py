e15ab6cb7d5bd4cee15f2328df7f2efbb8096ef5
André Silveira <andresilveira027@gmail.com>
2025-04-17 17:07:57 -0300

    start activity zmq

---
+++ Atividade ZMQ/server.py
@@ -0,0 +1,28 @@
+# Aluno: Andre Silveira Sousa RA: 628239
+
+import socket
+
+hostname = socket.gethostname()
+iplocal = socket.gethostbyname(hostname)
+print(iplocal)
+
+HOST = iplocal              # Endereco IP do Servidor
+PORT = 9999                 # Porta que o Servidor esta
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria um socket p/ internet via TCP
+endereco = (HOST, PORT)
+
+tcp.bind(endereco)          
+tcp.listen()  
+
+while True:
+    conexao, cliente = tcp.accept()
+    nick = conexao.recv(1024)
+    print(cliente , "conectou-se como ", nick.decode())
+    while True:
+        msg = conexao.recv(1024)
+        if not msg: break
+        print(nick.decode(), " enviou a mensagem: ", msg.decode())
+    print (nick.decode(), "desconectou-se.")
+    print("A conexao do cliente " , cliente, "foi encerrada.")
+    conexao.close()
