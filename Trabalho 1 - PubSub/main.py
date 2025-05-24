f48fb1e9f46c2697937fcabd2b24196c82970209
André Silveira <andresilveira027@gmail.com>
2025-05-24 20:45:34 -0300

    fix parse_args na main

--- Trabalho 1 - PubSub/main.py
+++ Trabalho 1 - PubSub/main.py
@@ -6,18 +6,31 @@ from audio import send_audio, receive_audio
 from video import send_video, receive_video
 from utils import parse_args
 
-nickname, listen_port, peer_endpoints, audio_peer_endpoints, video_peer_endpoints = parse_args()
+(
+    nickname,
+    listen_port,
+    peer_endpoints,
+    audio_peer_endpoints,
+    video_peer_endpoints,
+    use_audio,
+    use_video
+) = parse_args()
+
 context = zmq.Context()
 stop_event = threading.Event()
 
-# Recepção
+# Sempre ativa o canal de texto
 threading.Thread(target=receive_messages, args=(context, listen_port, stop_event), daemon=True).start()
-threading.Thread(target=receive_audio, args=(context, listen_port + 1000, stop_event), daemon=True).start()
-threading.Thread(target=receive_video, args=(context, listen_port + 2000, stop_event), daemon=True).start()
 
-# Envio
-threading.Thread(target=send_audio, args=(context, audio_peer_endpoints, stop_event), daemon=True).start()
-threading.Thread(target=send_video, args=(context, video_peer_endpoints, stop_event), daemon=True).start()
+# Ativa canais de áudio e vídeo somente se solicitados
+if use_audio:
+    threading.Thread(target=receive_audio, args=(context, listen_port + 1000, stop_event), daemon=True).start()
+    threading.Thread(target=send_audio, args=(context, audio_peer_endpoints, stop_event), daemon=True).start()
+
+if use_video:
+    threading.Thread(target=receive_video, args=(context, listen_port + 2000, stop_event), daemon=True).start()
+    threading.Thread(target=send_video, args=(context, video_peer_endpoints, stop_event), daemon=True).start()
+
 # Captura a saída limpa
 try:
     send_messages(context, nickname, peer_endpoints, stop_event)
