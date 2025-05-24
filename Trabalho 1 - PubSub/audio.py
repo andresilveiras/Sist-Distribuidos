5b0035e6aa637b7aa28b3e853b8a6b14759feaae
André Silveira <andresilveira027@gmail.com>
2025-05-24 18:41:25 -0300

    fix encerramento das threads de audio

--- Trabalho 1 - PubSub/audio.py
+++ Trabalho 1 - PubSub/audio.py
@@ -19,12 +19,21 @@ def send_audio(context, peer_audio_endpoints, stop_event):
     def callback(indata, frames, time_info, status):
         if status:
             print("Erro de entrada de áudio:", status)
-        pub_socket.send_multipart([pub_topic.encode(), indata.tobytes()])
+        try:
+            if not stop_event.is_set():
+                pub_socket.send_multipart([pub_topic.encode(), indata.tobytes()])
+        except zmq.ZMQError as e:
+            print("Erro ao enviar áudio:", e)
 
-    with sd.InputStream(samplerate=44100, channels=1, callback=callback, dtype='int16'):
-        while not stop_event.is_set():
-            time.sleep(0.1) # Envia pacotes de áudio a cada 100ms
-        pub_socket.close()    
+    try:
+        with sd.InputStream(samplerate=44100, channels=1, callback=callback, dtype='int16'):
+            while not stop_event.is_set():
+                time.sleep(0.1) # Envia pacotes de áudio a cada 0.1 segundos
+    except Exception as e:
+        print("Erro no stream de áudio:", e)
+    finally:
+        pub_socket.close()
+
 
 '''
 Função receive_audio
@@ -37,9 +46,14 @@ def receive_audio(context, listen_audio_port, stop_event):
     sub_socket.bind(f"tcp://0.0.0.0:{listen_audio_port}")
     sub_socket.setsockopt(zmq.SUBSCRIBE, sub_topic.encode())
 
-    while not stop_event.is_set():
-        topic, data_bytes = sub_socket.recv_multipart()
-        if topic.decode() == sub_topic:
-            audio_data = np.frombuffer(data_bytes, dtype='int16')
-            sd.play(audio_data, samplerate=44100)
-    sub_socket.close()
+    try:
+        while not stop_event.is_set():
+            topic, data_bytes = sub_socket.recv_multipart(flags=zmq.NOBLOCK)
+            if topic.decode() == sub_topic:
+                audio_data = np.frombuffer(data_bytes, dtype='int16')
+                sd.play(audio_data, samplerate=44100)
+            time.sleep(0.01)
+    except zmq.Again:
+        pass
+    finally:
+        sub_socket.close()
